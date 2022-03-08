8f33936288fbf403286efe6f8190dcb0254cf3b6
CollabDSO <98726763+CollabDSO@users.noreply.github.com>
2022-03-08 08:51:44 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World, Trying GitHub Webhook & Local Sync");
+		System.out.println("Hello World, Trying GitHub Webhook & Local Synchronization");
 	}
 }
